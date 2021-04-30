13eb1b99cd1247ebdeb722e9a2bcbbb6ca79982d
lukas <lukas.arnroth@gmail.com>
2021-04-30 23:06:44 +0200

    setup gibbs for m1

--- Tests/normalTest.jl
+++ Tests/normalTest.jl
@@ -5,22 +5,43 @@ y = rand(Normal(0, 2), 50)
 
 ## helpers
 function ESS(W::Vector{T})::T where {T <: Real}
-    1 / sum(W.^2)
+    weightSum = sum(W)
+    1/sum((W ./ weightSum).^2)
 end
 
 function α₁(Tₖ::N, t::N; p::T = 2.) where {N <: Integer, T <: Real}
     (t/Tₖ)^p
 end
 
+# πₜ^{2, k}(θₜ)|_{k=1} invariant kernel
+function μ_update(y::Vector{T}, σ::T, μ_0::T, σ_0::T, t::N, Tₖ::N) where {T<: Real, N <: Integer}
+    αₜ = α₁(Tₖ, t)
+    n = length(y)
+    μ_post = (σ_0 * mean(y) + σ/(n*αₜ) * μ_0) / (σ/(n*αₜ) + σ_0)
+    σ_post = 1/(n*αₜ / σ + 1/σ_0)
+    rand(Normal(μ_post, √σ_post), 1)[1]
+end
+
+μ_update(y, σ[1], μ₀, σ₀, 1, T)
+
+function σ_update(y::Vector{T}, μ::T, α₀::T, β₀::T, t::N, Tₖ::N) where {T<: Real, N <: Integer}
+    αₜ = α₁(Tₖ, t)
+    n = length(y)
+    α_post = α₀ + n*αₜ/2
+    β_post = β₀ + αₜ/2 * sum((y.- μ).^2)
+    rand(InverseGamma(α_post, β_post))
+end
+
+σ_update(y, μ[1], α₀, β₀, 1, T)
 
 
 ## initialization step
-N = 100
+N = 10
 θ = zeros(N, 2)
 T = 100
 # hyperparameters
-μ₀, σ₀ = 0, 10
-α₀, β₀ = 1, 2
+μ₀, σ₀ = 0., 10.
+α₀, β₀ = 1., 1.
 
 # initialize using ν ~ π(θ)
 σ = 1 ./ rand(Gamma(α₀,β₀), N)
@@ -35,19 +56,8 @@ end
 W = zeros(N, T)
 W[:, 1] .= 1/N
 
-prod(pdf.(Normal(μ[3], σ[3]), y)) ^ α₁(T, 1)
-
 for i in 1:N
-    W[i, 2] = W[i, 1] * prod(pdf.(Normal(μ[i], σ[i]), y)) ^ α₁(T, 1)
+    W[i, 2] = W[i, 1] * prod(pdf.(Normal(μ[i], √σ[i]), y)) ^ α₁(T, 1)
 end
-
-sum(W[:, 2].^2)
 
 ESS(W[:, 2])
-
-# step t
-for t in 2:T
-    for i in 1:N
-        lik = pdf.(Normal())
-    end
-end

577f13bf4a7226f765dd397f0cd04ef503707eac
lukketotte <lukas.arnroth@gmail.com>
2021-04-30 13:39:15 +0200

    got the first iteration working, only need posterior invariant kernel

--- Tests/normalTest.jl
+++ Tests/normalTest.jl
@@ -15,7 +15,7 @@ end
 
 
 ## initialization step
-N = 10
+N = 100
 θ = zeros(N, 2)
 T = 100
 # hyperparameters
@@ -26,11 +26,28 @@ T = 100
 σ = 1 ./ rand(Gamma(α₀,β₀), N)
 μ = zeros(N)
 for i in 1:N
-    μ[i] = rand(Normal(μ₀, σ₀ / σ[i]), 1)[1]
+    μ[i] = rand(Normal(μ₀, √(σ₀ / σ[i])), 1)[1]
 end
 
 θ = hcat(μ, σ)
 
 # weights
 W = zeros(N, T)
-W[:, 1] .= 1
+W[:, 1] .= 1/N
+
+prod(pdf.(Normal(μ[3], σ[3]), y)) ^ α₁(T, 1)
+
+for i in 1:N
+    W[i, 2] = W[i, 1] * prod(pdf.(Normal(μ[i], σ[i]), y)) ^ α₁(T, 1)
+end
+
+sum(W[:, 2].^2)
+
+ESS(W[:, 2])
+
+# step t
+for t in 2:T
+    for i in 1:N
+        lik = pdf.(Normal())
+    end
+end

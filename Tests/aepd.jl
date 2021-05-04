536f524e155b66ba7f1e1ce02ac4eab69fbddefd
lukas <lukas.arnroth@gmail.com>
2021-05-04 18:06:38 +0200

    aepd sampler added

---
+++ Tests/aepd.jl
@@ -0,0 +1,40 @@
+module AEPD
+
+export aepd
+
+using Distributions, LinearAlgebra, SpecialFunctions, Random
+import Base.rand
+
+struct aepd <: ContinuousUnivariateDistribution
+    μ::Real
+    σ::Real
+    p::Real
+    α::Real
+    aepd(μ, σ, p, α) = new(Real(μ), Real(σ), Real(p), Real(α))
+end
+
+function A(x, p, α)::Real
+    (1/2 + sign(x) * (1-α))^p
+end
+
+function δ(p, α)::Real
+    2*α^p*(1-α)^p / (α^p + (1-α)^p)
+end
+
+function pdf(d::aepd, x::Real)
+    del = δ(d.p, d.α)
+    a = A(x-d.μ, d.p, d.α)
+    C = del^(1/p) / (2^(1/p) * gamma(1+1/p))
+    d.σ * C * exp(-0.5 * del/a * abs((x - d.μ)/d.σ)^p)
+end
+
+function rand(rng::AbstractRNG, d::aepd)
+    del = δ(d.p, d.α)
+    if rand(rng) < d.α
+        d.μ + d.σ * 2^(1/d.p) * (-d.α * (rand(Gamma(1/d.p, 1))/del)^(1/d.p))
+    else
+        d.μ + d.σ * 2^(1/d.p) * ((1-d.α) * (rand(Gamma(1/d.p, 1))/del)^(1/d.p))
+    end
+end
+
+end
---
+++ Tests/tests.jl
@@ -0,0 +1,9 @@
+include("aepd.jl")
+using .AEPD
+using Distributions, LinearAlgebra, Plots, PlotThemes, KernelDensity
+theme(:juno)
+
+x = range(-5, 5, length = 500);
+d = aepd(0., 1., 2., 0.8);
+k = kde(rand(d, 10000));
+plot(x, pdf(k, x))

ec4b8521e7330b792e921fbb91a6ab772de3ca7c
lukketotte <lukas.arnroth@gmail.com>
2021-05-27 13:41:25 +0200

    conditional of shape is setup but seems slightly off

--- Tests/quantile/tests.jl
+++ Tests/quantile/tests.jl
@@ -3,14 +3,15 @@ using Plots, PlotThemes
 theme(:juno)
 
 ## generate data
-n = 100
+n = 50
 β = [2.1, 0.8]
-α, θ = 0.5, 1.5
+α, θ, σ = 0.5, 2., 1.
 x₁ = rand(Normal(0, 5), n)
 repeat([1], 100)
 x₂ = rand(Normal(2, 5), n)
-X = [x₁ x₂]
-y = X * β .+ rand(Normal(0, 2), n)
+X = [repeat([1], n) x₂]
+y = X * β .+ rand(Normal(0, σ), n)
+
 
 function δ(α::T, θ::T)::T where {T <: Real}
     2*(α*(1-α))^θ / (α^θ + (1-α)^θ)
@@ -47,17 +48,17 @@ end
 function θinterval(X::Array{T, 2}, y::Array{T, 1}, u₁::Array{T, 1}, u₂::Array{T, 1},
     β::Array{T, 1}, α::T, σ::T) where {T <: Real}
     n = length(y)
-    θ = range(0.01, 3, length = 2000)
+    θ = range(0.01, 3, length = 2500)
     θₗ, θᵤ = zeros(n), zeros(n)
     for i in 1:n
         if u₁[i] > 0
-            θside = (u₁[i] .^ (1 ./ θ)) ./ gamma(1 + 1 ./ θ)
+            θside = (u₁[i] .^ (1 ./ θ)) ./ gamma.(1 .+ 1 ./ θ)
             c = (X[i,:] ⋅ β - y[i]) / (α * σ)
             θᵤ[i] = θ[maximum(findall(θside .> c))]
             θₗ[i] = θ[minimum(findall(θside .> c))]
         else
-            θside = (u₂[i] .^ (1 ./ θ)) ./ gamma(1 + 1 ./ θ)
-            c = (y[i] - X[i,:] ⋅ β) / (α * σ)
+            θside = (u₂[i] .^ (1 ./ θ)) ./ gamma.(1 .+ 1 ./ θ)
+            c = (y[i] - X[i,:] ⋅ β) / ((1-α) * σ)
             θᵤ[i] = θ[maximum(findall(θside .> c))]
             θₗ[i] = θ[minimum(findall(θside .> c))]
         end
@@ -65,6 +66,32 @@ function θinterval(X::Array{T, 2}, y::Array{T, 1}, u₁::Array{T, 1}, u₂::Arr
     [maximum(θₗ) minimum(θᵤ)]
 end
 
+function θcond(θ::T, u₁::Array{T, 1}, u₂::Array{T, 1}, α::T) where {T <: Real}
+    n = length(u₁)
+    n*(1+1/θ) * log(δ(α, θ)) - δ(α, θ) * sum(u₁ .+ u₂)
+end
+
+function sampleθ(θ::T, X::Array{T, 2}, y::Array{T, 1}, u₁::Array{T, 1}, u₂::Array{T, 1},
+    β::Array{T, 1}, α::T, σ::T) where {T <: Real}
+    interval = θinterval(X, y, u₁, u₂, β, α, σ)
+    prop = rand(Uniform(minimum(interval), maximum(interval)), 1)[1]
+    θcond(prop, u₁, u₂, α) - θcond(θ, u₁, u₂, α) >= log(rand(Uniform(0,1), 1)[1]) ? prop : θ
+    prop
+end
+
+
+## conditional density seems to be centered 0.5 too high
+u1, u2 = sampleLatent(X, y, [2.1, 0.8], α, 2., 1.)
+θinterval(X, y, u1, u2, β, α, σ) |> println
+
+n*(1+1/θ) * log(δ(α, θ)) - δ(α, θ) * sum( u1 .+ u2)
+
+p = range(0.5, 4., length = 200)
+pl = [θcond(a, u1, u2, 0.73)  for a in p]
+plot(p, pl, legend = false)
+
+
+##
 
 function sampleβ(X::Array{T, 2}, y::Array{T, 1}, u₁::Array{T, 1}, u₂::Array{T, 1},
     β::Array{T, 1}, α::T, θ::T, σ::T, τ::T) where {T <: Real}
@@ -106,22 +133,26 @@ end
 
 
 
-nMCMC = 20000
+nMCMC = 2000
 σ = zeros(nMCMC)
 σ[1] = 1
 β = zeros(nMCMC, 2)
 β[1, :] = [2.1, 0.8]
-
+θ = zeros(nMCMC)
+θ[1] = 2.
 
 for i in 2:nMCMC
-    (i % 500 === 0) && println(i)
-    u1, u2 = sampleLatent(X, y, β[i-1,:], α, θ, σ[i-1])
-    β[i,:] = sampleβ(X, y, u1, u2, β[i-1,:], α, θ, σ[i-1], 10.)
-    σ[i] = sampleSigma(X, y, u1, u2, β[i-1, :], α, θ, 1)
+    (i % 250 === 0) && println(i)
+    u1, u2 = sampleLatent(X, y, β[i-1,:], α, θ[i-1], σ[i-1])
+    β[i,:] = sampleβ(X, y, u1, u2, β[i-1,:], α, θ[i-1], σ[i-1], 10.)
+    σ[i] = sampleSigma(X, y, u1, u2, β[i, :], α, θ[i-1], 1)
+    θ[i] = sampleθ(θ[i-1], X, y, u1, u2, β[i, :], α, σ[i])
 end
 
-plot(β[:, 1])
+plot(β[:, 2])
 plot(σ)
+plot(θ)
+mean(θ)
 
 plot(cumsum(σ) ./ (1:nMCMC))
 plot(cumsum(β[:, 2]) ./ (1:nMCMC))

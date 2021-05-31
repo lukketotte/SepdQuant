0df18dd3a4f0267144fd1c23133304954589139d
lukketotte <lukas.arnroth@gmail.com>
2021-05-31 15:19:21 +0200

    not really working

--- Tests/quantile/tests.jl
+++ Tests/quantile/tests.jl
@@ -3,14 +3,16 @@ using Plots, PlotThemes
 theme(:juno)
 
 ## generate data
-n = 100
+n = 1000
 β = [2.1, 0.8]
 α, θ, σ = 0.5, 2., 1.
 x₁ = rand(Normal(0, 5), n)
-repeat([1], 100)
-x₂ = rand(Normal(2, 5), n)
+x₂ = rand(Uniform(0, 20), n)
 X = [repeat([1], n) x₂]
 y = X * β .+ rand(Normal(0, σ), n)
+
+
+√(var(y - X * inv(X' * X) * X' * y))
 
 function δ(α::T, θ::T)::T where {T <: Real}
     2*(α*(1-α))^θ / (α^θ + (1-α)^θ)
@@ -35,15 +37,6 @@ end
 
 function sampleSigma(X::Array{T, 2}, y::Array{T, 1}, u₁::Array{T, 1}, u₂::Array{T, 1},
     β::Array{T, 1}, α::T, θ::T, ν::N = 1) where {T, N <: Real}
-    u1Pos = u₁ .> 0
-    u2Pos = u₂ .> 0
-    l₁ = maximum((X[u1Pos,:] * β .- y[u1Pos]) .* gamma(1+1/θ) ./ (α .* u₁[u1Pos].^(1/θ)))
-    l₂ = maximum((y[u2Pos] .- X[u2Pos,:] * β) .* gamma(1+1/θ) ./ ((1-α) .* u₂[u2Pos].^(1/θ)))
-    rand(Pareto(ν + length(y) - 1, maximum([l₁ l₂])), 1)[1]
-end
-
-function sampleSigma(X::Array{T, 2}, y::Array{T, 1}, u₁::Array{T, 1}, u₂::Array{T, 1},
-    β::Array{T, 1}, α::T, θ::T, ν::N = 1) where {T, N <: Real}
     n = length(y)
     lower = zeros(n)
     for i in 1:n
@@ -57,10 +50,13 @@ function sampleSigma(X::Array{T, 2}, y::Array{T, 1}, u₁::Array{T, 1}, u₂::Ar
     rand(Pareto(ν + length(y) - 1, maximum(lower)), 1)[1]
 end
 
+u1, u2 = sampleLatent(X, y, β, α, θ, σ)
+sampleSigma(X, y, u1, u2, β, α, θ, 1)
+
 function θinterval(X::Array{T, 2}, y::Array{T, 1}, u₁::Array{T, 1}, u₂::Array{T, 1},
     β::Array{T, 1}, α::T, σ::T) where {T <: Real}
     n = length(y)
-    θ = range(0.01, 4, length = 2500)
+    θ = range(0.01, 5, length = 3000)
     θₗ, θᵤ = zeros(n), zeros(n)
     for i in 1:n
         if u₁[i] > 0
@@ -91,57 +87,6 @@ function sampleθ(θ::T, X::Array{T, 2}, y::Array{T, 1}, u₁::Array{T, 1}, u₂
     minimum(interval) >= maximum(interval) ? minimum(interval) : rand(Uniform(minimum(interval), maximum(interval)), 1)[1]
 end
 
-
-## conditional density seems to be centered 0.5 too high
-u1, u2 = sampleLatent(X, y, [2.1, 0.8], α, 2., 1.)
-θinterval(X, y, u1, u2, β, α, σ) |> println
-
-n*(1+1/θ) * log(δ(α, θ)) - δ(α, θ) * sum( u1 .+ u2)
-
-p = range(0.5, 4., length = 200)
-pl = [θcond(a, u1, u2, 0.73)  for a in p]
-plot(p, pl, legend = false)
-
-##
-
-function sampleβ(X::Array{T, 2}, y::Array{T, 1}, u₁::Array{T, 1}, u₂::Array{T, 1},
-    β::Array{T, 1}, α::T, θ::T, σ::T, τ::T) where {T <: Real}
-    n, p = size(X)
-    βsim = zeros(p)
-
-    for k in 1:p
-        u1Pos = (X[:, k] .< 0) .& (u₁ .> 0)
-        u2Pos = (X[:, k] .> 0) .& (u₂ .> 0)
-        l = []
-        if sum(u1Pos) > 0
-            a1 = (y[u1Pos] .- X[u1Pos, 1:end .!= k] * β[1:end .!= k]) ./ X[u1Pos, k]
-            b1 = σ*α ./ X[u1Pos, k] .* u₁[u1Pos].^(1/θ) ./ gamma(1 + 1/θ)
-            l = append!(l, a1.+b1)
-        end
-        if sum(u2Pos) > 0
-            a2 = (y[u2Pos] .- X[u2Pos, 1:end .!= k] * β[1:end .!= k]) ./ X[u2Pos, k]
-            b2 = σ*(1-α) ./ X[u2Pos, k] .* u₂[u2Pos].^(1/θ) ./ gamma(1 + 1/θ)
-            l = append!(l, a2.-b2)
-        end
-        u1Pos = (X[:, k] .> 0) .& (u₁ .> 0)
-        u2Pos = (X[:, k] .< 0) .& (u₂ .> 0)
-        u = []
-        if sum(u1Pos) > 0
-            a1 = (y[u1Pos] .- X[u1Pos, 1:end .!= k] * β[1:end .!= k]) ./ X[u1Pos, k]
-            b1 = σ*α ./ X[u1Pos, k] .* u₁[u1Pos].^(1/θ) ./ gamma(1 + 1/θ)
-            u = append!(u, a1.+b1)
-        end
-        if sum(u2Pos) > 0
-            a2 = (y[u2Pos] .- X[u2Pos, 1:end .!= k] * β[1:end .!= k]) ./ X[u2Pos, k]
-            b2 = σ*(1-α) ./ X[u2Pos, k] .* u₂[u2Pos].^(1/θ) ./ gamma(1 + 1/θ)
-            u = append!(u, a2.-b2)
-        end
-
-        βsim[k] = maximum(l) < minimum(u) ? rand(truncated(Normal(0, τ), maximum(l), minimum(u)), 1)[1] : maximum(l)
-    end
-    βsim
-end
-
 function sampleβ(X::Array{T, 2}, y::Array{T, 1}, u₁::Array{T, 1}, u₂::Array{T, 1},
     β::Array{T, 1}, α::T, θ::T, σ::T, τ::T) where {T <: Real}
     n, p = size(X)
@@ -168,14 +113,7 @@ function sampleβ(X::Array{T, 2}, y::Array{T, 1}, u₁::Array{T, 1}, u₂::Array
     βsim
 end
 
-u1, u2 = sampleLatent(X, y, β, α, θ, σ)
-sampleβ(X, y, u1, u2, β, α, θ, σ, 10.)
-
-k = 1
-i = 2
-
-
-nMCMC = 1000
+nMCMC = 500
 σ = zeros(nMCMC)
 σ[1] = 1
 β = zeros(nMCMC, 2)
@@ -187,14 +125,13 @@ simU2 = zeros(nMCMC, n)
 
 for i in 2:nMCMC
     (i % 250 === 0) && println(i)
-    # print(i)
     u1, u2 = sampleLatent(X, y, β[i-1,:], α, θ[i-1], σ[i-1])
     simU1[i,:] = u1
     simU2[i,:] = u2
     β[i,:] = sampleβ(X, y, u1, u2, β[i-1,:], α, θ[i-1], σ[i-1], 10.)
-    σ[i] = sampleSigma(X, y, u1, u2, β[i, :], α, θ[i-1], 1)
-    # σ[i] = 1.
-    θ[i] = sampleθ(θ[i-1], X, y, u1, u2, β[i, :], α, σ[i])
+    θ[i] = sampleθ(θ[i-1], X, y, u1, u2, β[i, :], α, σ[i-1])
+    # θ[i] = 2.
+    σ[i] = sampleSigma(X, y, u1, u2, β[i, :], α, θ[i], 1)
 end
 
 
@@ -203,38 +140,8 @@ plot(β[:, 2])
 plot(σ)
 plot(θ)
 
-
 plot(cumsum(σ) ./ (1:nMCMC))
-plot(cumsum(β[:, 2]) ./ (1:nMCMC))
+plot(cumsum(β[:, 1]) ./ (1:nMCMC))
 plot(cumsum(θ) ./ (1:nMCMC))
 
-mean(σ) * √(gamma(1+1/θ))
-
-σ
-
-i = 3
-sampleSigma(X, y, simU1[i,:], simU2[i,:], β[i, :], α, θ[i-1], 1)
-
-lower = zeros(n)
-for j in 1:n
-    μ = X[j,:] ⋅ β[i,:]
-    if y[j] <= μ
-        lower[j] = (μ - y[j]) * gamma(1+1/θ[i-1]) / (α * simU1[i,j]^(1/θ[i-1]))
-    else
-        lower[j] = (y[j] - μ) * gamma(1+1/θ[i-1]) / ((1-α) * simU2[i,j]^(1/θ[i-1]))
-    end
-end
-
-findall(lower .=== Inf)
-
-j = 42
-
-(y[j] - μ) * gamma(1+1/θ[i-1]) / ((1-α) * simU2[i,j]^(1/θ[i-1]))
-
-maximum(lower)
-rand(Pareto(1 + length(y) - 1, maximum(lower)), 1)[1]
-
-sampleβ(X, y, simU1[i,:], simU2[i,:], β[i-1,:], α, θ[i-1], σ[i-1], 10.)
-sum(simU1[i,:])
-sum(simU2[i,:])
-β[i-2, :]
+√var(y)

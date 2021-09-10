4ab32a22d0e0d514410751df7a90ade6ad4398b9
lukketotte <lukas.arnroth@gmail.com>
2021-09-10 12:29:54 +0200

    everything works ok

--- Tests/quantile/ConflictDat.jl
+++ Tests/quantile/ConflictDat.jl
@@ -22,41 +22,16 @@ X = X[y.>0,:];
 y = y[y.>0];
 X = hcat([1 for i in 1:length(y)], X);
 
-colnames = names(dat)
-colnames[1] = "intercept"
-
-
 par = MCMCparams(y, X, 1000, 5, 1);
+α = 0.9
+par = MCMCparams(log.(y + rand(Uniform(), length(y)) .- α), X, 100000, 5, 1);
 βinit = [-0.48, -0.14, -2.6, 3.7, 0., 0.1, 1.75, -0.05, 0.28]
-β, θ, σ = mcmc(par, 0.9, 100., .8, .25, βinit, 3, 1.5, true);
+β, θ, σ = mcmc(par, α, 100., .8, .25, βinit, 3, 1.5, true);
 
-
-
-hcat(DataFrame(β, colnames), DataFrame([σ, θ], ["σ", "θ"]))
-
-
-convert(DataFrame, β)
-DataFrame(β, colnames)
-DataFrame(β, :auto)
 p = 2
 plot(β[:,p])
 plot!(1:length(θ), cumsum(β[:,p])./(1:length(θ)))
-plot!(β1[:,p])
-plot!(1:length(θ), cumsum(β1[:,p])./(1:length(θ)))
-
-p = 9
-plot(1:length(θ), cumsum(β2[:,p])./(1:length(θ)), label = "α = 0.1")
-plot!(1:length(θ), cumsum(β[:,p])./(1:length(θ)), label = "α = 0.5")
-plot!(1:length(θ), cumsum(β1[:,p])./(1:length(θ)), label = "α = 0.9")
-
-
 1-((β[2:length(θ), 1] .=== β[1:(length(θ) - 1), 1]) |> mean)
-round.([median(β[:,i]) for i in 1:9], digits = 3) |> println
-1-((θ[2:length(θ)] .=== θ[1:(length(θ) - 1), 1]) |> mean)
-plot(θ, label = "α = 0.5")
-plot!(θ1, label = "α = 0.9")
-plot!(σ, label = "σ")
-
 
 ## Estimate over multiple quantiles
 colnames = names(dat)
@@ -70,4 +45,3 @@ for a in α
     β, θ, σ = mcmc(par, 0.9, 100., .8, .25, βinit, 3, 1.5, true)
     CSV.write(format(feMCMC, 0.9), hcat(DataFrame(β, colnames), DataFrame([σ, θ], ["σ", "θ"])))
 end
-# format(fe, 0.05)

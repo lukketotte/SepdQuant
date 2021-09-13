eca6965c2800df2038fc1083bb09a36d4a1dcecc
lukketotte <lukas.arnroth@gmail.com>
2021-09-13 14:32:43 +0200

    set up for runs over M

--- Tests/quantile/MultiProc.jl
+++ Tests/quantile/MultiProc.jl
@@ -16,46 +16,34 @@ X = X[y.>0,:];
 y = y[y.>0];
 X = hcat([1 for i in 1:length(y)], X);
 
-par = Sampler(y, X, 0.5, 100, 1, 1);
 βinit = [-0.48, -0.14, -2.6, 3.7, 0., 0.1, 1.75, -0.05, 0.28]
-
-M = 5
-par = Sampler(y, X, 0.5, 200000, 5, 100000);
-
-N = Integer((par.nMCMC - par.burnIn)/par.thin) + 1
-
-β = SharedArray{Float64}((N, size(X)[2], M))
-σ = SharedArray{Float64}((N,M))
-θ = SharedArray{Float64}((N,M))
-
+M = 50
+p = 0.05
+feMCMC = FormatExpr("mcmc_{}.csv")
 α = range(0.1, 0.9, length = 9)
 
-@distributed for i = 1:20
-    βt,θt,σt = mcmc(par, 100., .8, .25, βinit, 0.5, 1.)
-    β[:,:,i] = βt
-    θ[:,i] = θt
-    σ[:,i] = σt
+for i in 1:length(α)
+    par = Sampler(y, X, α[i], 200000, 5, 100000);
+    N = Integer((par.nMCMC - par.burnIn)/par.thin) + 1
+    β = SharedArray{Float64}((N, size(X)[2], M))
+    σ = SharedArray{Float64}((N,M))
+    θ = SharedArray{Float64}((N,M))
+
+    @sync @distributed for i = 1:M
+        βt,θt,σt = mcmc(par, 100., .8, .25, βinit, 1., 1.)
+        β[:,:,i] = βt
+        θ[:,i] = θt
+        σ[:,i] = σt
+    end
+
+    a = DataFrame(param = colnames,
+        value =[mean([mean(β[:, j, i]) for i in 1:M]) for j in 1:size(X)[2]],
+        l = [mean([sort(β, dims = 1)[Integer(round((p/2) * N)), j, i] for i in 1:M]) for j in 1:size(X)[2]],
+        u =  [mean([sort(β, dims = 1)[Integer(round((1-p/2) * N)), j, i] for i in 1:M]) for j in 1:size(X)[2]], α = α[i])
+
+    b = DataFrame(param = ["sigma", "theta"], value = [mean([mean(σ[:, i]) for i in 1:M]), mean([mean(θ[:, i]) for i in 1:M])],
+            l = [mean(sort(σ, dims = 1)[Integer(round((p/2) * N)), :]) ; mean(sort(θ, dims = 1)[Integer(round((p/2) * N)), :])],
+            u = [mean(sort(σ, dims = 1)[Integer(round((1-p/2) * N)), :]); mean(sort(θ, dims = 1)[Integer(round((1-p/2) * N)), :])], α = α[i])
+
+    CSV.write(format(feMCMC, α[i]), vcat(a, b))
 end
-
-mean([mean(θ[:, i]) for i in 1:M])
-[mean([mean(β[:, j, i]) for i in 1:M]) for j in 1:size(X)[2]]
-[mean([√var(β[:, j, i]) for i in 1:M]) for j in 1:size(X)[2]]
-
-colnames =["intercept" ; names(dat[:,2:9])]
-
-
-"sig_" .* colnames
-
-append!(colnames, "σ_" .* colnames)
-
-[[mean([mean(β[:, j, i]) for i in 1:M]) for j in 1:size(X)[2]] ; [mean([√var(β[:, j, i]) for i in 1:M]) for j in 1:size(X)[2]]]
-
-a = DataFrame(param = colnames,
-    value =[mean([mean(β[:, j, i]) for i in 1:M]) for j in 1:size(X)[2]],
-    sd = [mean([√var(β[:, j, i]) for i in 1:M]) for j in 1:size(X)[2]], α = 0.5)
-
-b = DataFrame(param = ["sigma", "theta"], value = [mean([mean(σ[:, i]) for i in 1:M]), mean([mean(θ[:, i]) for i in 1:M])],
-    sd = [mean([√var(σ[:, i]) for i in 1:M]), mean([√var(θ[:, i]) for i in 1:M])], α = 0.5)
-
-print(b)
-vcat(a, b)

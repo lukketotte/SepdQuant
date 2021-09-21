3366cb43ede8b1344ca6f30c47a95c04b1bd17bf
lukketotte <lukas.arnroth@gmail.com>
2021-09-21 16:15:25 +0200

    comparing HMC and MALA

--- Tests/quantile/debug.jl
+++ Tests/quantile/debug.jl
@@ -1,100 +1,72 @@
 using Distributions, LinearAlgebra, StatsBase, SpecialFunctions, ForwardDiff
 using Plots, PlotThemes, CSV, DataFrames, StatFiles, CSVFiles
 theme(:juno)
+using Turing, QuantileRegressions
 include("../../QuantileReg/QuantileReg.jl")
-using .QuantileReg
+include("../aepd.jl")
+using .QuantileReg, .AEPD
 
-dat = load(string(pwd(), "/Tests/data/hks_jvdr.csv")) |> DataFrame;
-y = dat[:, :osvAll]
-X = dat[:, Not(["osvAll"])] |> Matrix
-X = X[y.>0,:];
-y = y[y.>0];
-X = hcat([1 for i in 1:length(y)], X);
-Z = X[:, [1, 4, 7, 8, 9]]
 
-names(dat)
-## try with θ as a linear model
-βinit = [-1.85, -0.046, -2.477, 2.628, 0.0, 0.412, 1.522, -0.003, 0.206]
-par = Sampler(y, X,0.5, 10000, 1, 1);
+n = 200;
+β, α, σ = [2.1, 0.8], 0.5, 2.;
+θ =  1.
+X = [repeat([1], n) rand(Uniform(10, 20), n)]
+y = X * β .+ rand(Aepd(0., σ, θ, 0.5), n);
 
+##
 function δ(α::Real, θ::Real)
     return 2*(α*(1-α))^θ / (α^θ + (1-α)^θ)
 end
 
-function δ(α::Real, θ::AbstractVector{<:Real})
-    return 2*(α*(1-α)) .^ θ ./ (α .^ θ + (1-α) .^ θ)
-end
-
-z  = par.y-par.X*βinit
-η = Z * [1, 0.1, 0.001, 0.1]
-
-a = (sum(δ(par.α, η[z.<0]).*(.-z[z.<0]).^η[z.<0]/par.α.^η[z.<0]) + sum(δ(par.α, η[z.>=0]).*z[z.>=0].^η[z.>=0] /(1-par.α).^η[z.>=0]))
-n = length(z)
-sum(n./η .* log.(δ(par.α, η))  .- n*log.(gamma.(1 .+1 ./η)) .- n*log(a)./η .+ loggamma.(n./η))
-
-
-function θcond(θ::AbstractVector{<:Real}, s::Sampler, Z::AbstractMatrix{<:Real}, β::AbstractVector{<:Real})
-    z  = s.y-s.X*β
-    η = exp.(Z * θ)
-    n = length(z)
-    neg = z .< 0
-    pos = z .>= 0
-    a = (sum(δ(par.α, η[neg]).*(.-z[neg]).^η[neg]/par.α.^η[neg]) + sum(δ(par.α, η[pos]).*z[pos].^η[pos] /(1-par.α).^η[pos]))
-    return sum(n./η .* log.(δ(par.α, η))  .- n*log.(gamma.(1 .+1 ./η)) .- n*log(a)./η .+ loggamma.(n./η))
-end
-
-function sampleθ(s::Sampler, θ::AbstractVector{<:Real}, Z::AbstractMatrix{<:Real}, β::AbstractVector{<:Real}, ε::Real)
-    ∇ = ∂θ(θ, s, β, Z)
-    #H = ∂θ2(θ, s, β, Z)^(-1) |> Symmetric
-    prop = θ + (ε^2 / 2)  * ∇ + ε * vec(rand(MvNormal(zeros(length(θ)), 1), 1))
-    ∇ₚ = ∂θ(prop, s, β, Z)
-    #Hₚ = ∂θ2(prop, s, β, Z)^(-1) |> Symmetric
-    α = θcond(prop, s, Z, β) - θcond(θ, s, Z, β)
-    α += -logpdf(MvNormal(θ + (ε^2 / 2) * ∇, ε^2), prop) + logpdf(MvNormal(prop + (ε^2 / 2)* ∇ₚ, ε^2), θ)
-    return log(rand(Uniform(0,1), 1)[1]) <= α ? prop : θ
-end
-
-function sampleθ(s::Sampler, θ::AbstractVector{<:Real}, Z::AbstractMatrix{<:Real},
-    β::AbstractVector{<:Real}, ε::AbstractVector{<:Real})
-    ∇ = ∂θ(θ, s, β, Z)
-    prop = θ + vec(rand(MvNormal(zeros(length(θ)), diagm(ε)), 1))
-    α = θcond(prop, s, Z, β) - θcond(θ, s, Z, β)
-    return log(rand(Uniform(0,1), 1)[1]) <= α ? prop : θ
-end
-
-function sampleσ(s::Sampler, θ::AbstractVector{<:Real}, Z::AbstractMatrix{<:Real}, β::AbstractVector{<:Real})
+function logβCond(β::AbstractVector{<:Real}, s::Sampler, θ::Real, σ::Real, τ::Real, λ::AbstractVector{<:Real})
     z = s.y - s.X*β
-    η = exp.(Z * θ)
-    b = sum(δ(s.α, η[z.<0]) .* (.-z[z.<0]).^ η[z.<0] ./ s.α.^η[z.<0])
-    b += sum(δ(s.α, η[z.>=0]) .* (z[z.>=0].^η[z.>=0] ./ (1-s.α).^η[z.>=0]))
-    return rand(InverseGamma(sum(1/η) + 1, b + 1), 1)[1]
+    b = δ(s.α, θ)/σ * (sum((.-z[z.< 0]).^θ) / s.α^θ + sum(z[z.>=0].^θ) / (1-s.α)^θ)
+    return -b -1/(2*τ) * β'*diagm(λ.^(-2))*β
 end
 
-ε^2 .* vec(rand(MvNormal(zeros(4), 1), 1))
+# first derivative
+∂β(β::AbstractVector{<:Real}, s::Sampler, θ::Real, σ::Real, τ::Real, λ::AbstractVector{<:Real}) = ForwardDiff.gradient(β -> logβCond(β, s, θ, σ, τ, λ), β)
+∂β2(β::AbstractVector{<:Real}, s::Sampler, θ::Real, σ::Real, τ::Real, λ::AbstractVector{<:Real}) = ForwardDiff.jacobian(β -> -∂β(β, s, θ, σ, τ, λ), β)
 
-∂θ(θ::AbstractVector{<:Real}, s::Sampler, β::AbstractVector{<:Real}, Z::AbstractMatrix{<:Real}) = ForwardDiff.gradient(θ -> θcond(θ, s, Z, β), θ)
-∂θ2(θ::AbstractVector{<:Real}, s::Sampler, β::AbstractVector{<:Real}, Z::AbstractMatrix{<:Real}) = ForwardDiff.jacobian(θ -> -∂θ(θ, s, β, Z), θ)
+##
+diagm([1; [10 for i in 1:5]])^(-1)
 
-∂θ2(θ[N,:], par, βinit, Z)^(-1)
-
-N = 20000
-σ = zeros(N)
-θ = zeros(N, size(Z)[2])
-σ[1] = 2.
-θinit = θ[N,:]
-θ[1,:] = [.01 for i in 1:size(Z)[2]]
-θ[1,:] = θinit
-ε = [0.00000001, 0.001, 0.0001, 0.00000001, 0.0001]
-for i in 2:N
-    θ[i,:] = sampleθ(par, θ[i-1,:], Z, βinit, ε)
-    # σ[i] = sampleσ(par, θ[i,:], Z, βinit)
+function hmc(β::AbstractArray{Float64}, θ::Real, σ::Real, τ::Real, s::Sampler; L = 40, ϵ = 0.001, p = size(s.X, 2))
+    λ = abs.(rand(Cauchy(0,1), p))
+    #M = diagm([1; [10 for i in 1:(p-1)]])^(-1) |> Symmetric
+    M = diagm([.3, 1, .5, .7, 1000, 1, .5, 1000, .1])^(-1)
+    ϕ = reshape(rand(MvNormal(zeros(p), M), 1), p) + 0.5 * ϵ * ∂β(β, s, θ, σ, τ, λ)
+    β₁, ϕ₁ = β, ϕ
+    log_p = logβCond(β₁, s, θ, σ, τ, λ) - ϕ₁'*M*ϕ₁/2 # sum(ϕ₁.^2)/2
+    for l ∈ 1:L
+        ϕ += 0.5 * ϵ * ∂β(β, s, θ, σ, τ, λ)
+        β += ϵ * M * ϕ
+        ϕ += 0.5 * ϵ * ∂β(β, s, θ, σ, τ, λ)
+    end
+    log_p_L = logβCond(β, s, θ, σ, τ, λ) - ϕ'*M*ϕ/2 # sum(ϕ.^2)/2
+    r = exp(log_p_L - log_p)
+    r > rand(Uniform()) ? β : β₁
 end
-1-((θ[2:length(σ), 1] .=== θ[1:(length(σ) - 1), 1]) |> mean)
-plot(θ[:,3])
 
-median(σ)
-plot(σ)
-plot(θ[:,5])
+par = Sampler(y, X, 0.5, 1000, 1, 1);
+inits = coef(qreg(@formula(y ~ x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9), hcat(DataFrame(y = log.(y)), DataFrame(X, :auto)), 0.5))
 
-[median(θ[:,i]) for i in 1:size(Z)[2]]
-exp.(Z*[median(θ[:,i]) for i in 1:size(Z)[2]]) |> maximum
+N = 4001
+b = zeros(N, size(X,2))
+b[1,:] = inits
+for i ∈ 2:N
+    b[i,:] = hmc(b[i-1,:], 1.8, 4.5, 100, par; L = 40, ϵ = 0.001)
+end
+p = 8
+plot(β[:,p])
+plot!(b[:,p])
+
+p = 1
+plot(1:N, cumsum(b[:,p])./(1:N))
+plot!(1:N, cumsum(β[:,p])./(1:N))
+
+1-((b[2:N, 2] .=== b[1:(N - 1), 2]) |> mean)
+chain = Chains(b, ["intercept";names(dat[:, Not(["osvAll"])])]);
+mean(summarystats(chain)[:, :ess]) / N
+[median(b[:,i]) for i in 1:size(X,2)]
+println(inits)
--- Tests/quantile/turing.jl
+++ Tests/quantile/turing.jl
@@ -15,21 +15,47 @@ X = X[y.>0,:];
 y = log.(y[y.>0] + rand(Uniform(0,1), size(X,1)));
 X = hcat([1 for i in 1:length(y)], X);
 
+## they all behave similarly with this data at least.
+n = 200;
+β, α, σ = [2.1, 0.8], 0.5, 2.;
+θ =  1.
+X = [repeat([1], n) rand(Uniform(10, 20), n)]
+y = X * β .+ rand(Aepd(0., σ, θ, 0.5), n);
+
 @model apdreg(X, y; predictors=size(X,2)) = begin
     # priors
-    θ = 1.8#~ Uniform(0.01,3)
-    # σ ~ InverseGamma(1, 1)
-    σ = 4
+    θ ~ Uniform(0.5,3)
+    σ ~ InverseGamma(1, 1)
     β ~ filldist(Normal(0., 100.), predictors)
     for i in 1:length(y)
-        y[i] ~ Aepd(-2.1 + X[i,:] ⋅ β, σ, θ, 0.5)
+        y[i] ~ Aepd(X[i,:] ⋅ β, σ, θ, 0.5)
     end
 end
 
-model = apdreg(X, log.(y));
-chain = sample(model, NUTS(1000, 0.65), 5000, init_theta = inits[2:length(inits)]);
+model = apdreg(X, y);
+chain = sample(model, NUTS(), 3000);
 # chain = sample(model, Gibbs(PG(100, :σ), HMC(.03, 10, :β)), 1000);
 summaries = summarystats(chain)
-plot(chain[:"β[7]"])
-
+plot(chain[:"β[1]"])
+plot!(β[1:length(chain[:"β[1]"]),1])
 println(inits)
+
+plot(chain[:"θ"])
+plot!(θ[1:length(chain[:"θ"])])
+
+plot(chain[:"σ"])
+plot!(σ[1:length(chain[:"σ"])])
+
+using QuantileRegressions
+coef(qreg(@formula(y ~ x2), hcat(DataFrame(y = y), DataFrame(X, :auto)), 0.5))
+
+
+par = Sampler(y, X, 0.5, 50000, 10, 20000);
+β, θ, σ = mcmc(par, 100., .05, .07, [2.5, 0.75], 1., 2.);
+[median(β[:,i]) for i in 1:2]
+
+1-((β[2:length(θ), 1] .=== β[1:(length(θ) - 1), 1]) |> mean)
+1-((θ[2:length(θ)] .=== θ[1:(length(θ) - 1)]) |> mean)
+plot(β[:,1])
+plot(θ)
+plot(σ)

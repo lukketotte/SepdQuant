15009085ca504bbffdbc5fd08eb8b04808d78185
lukketotte <lukas.arnroth@gmail.com>
2021-09-24 14:35:29 +0200

    setting up individual proposal variances

--- Tests/quantile/ConflictDat.jl
+++ Tests/quantile/ConflictDat.jl
@@ -1,4 +1,4 @@
-using Distributions, LinearAlgebra, StatsBase, SpecialFunctions
+using Distributions, LinearAlgebra, StatsBase, SpecialFunctions, Turing
 include("../aepd.jl")
 include("../../QuantileReg/QuantileReg.jl")
 using .AEPD, .QuantileReg
@@ -17,35 +17,25 @@ X = hcat([1 for i in 1:length(y)], X);
 
 # par = Sampler(y, X, 0.5, 150000, 20, 10000);
 
-# α = 0.1: thin = 10, ϵ = 0.5
-# α = 0.2: thin = 10, ϵ = 0.7
-# α = 0.3: thin = 10, ϵ = 0.9
-# α = 0.4: thin = 10, ϵ = 1.
-# α = 0.5: thin = 10, ϵ = 1.
-# α = 0.6: thin = 10, ϵ = 1.
-# α = 0.7: thin = 10, ϵ = 1.
-# α = 0.8: thin = 10, ϵ = 1.2
-# α = 0.9: thin = 10, ϵ = .5
+# α = 0.1: thin = 30, ϵ = .9
+# α = 0.2: thin = 30, ϵ = 1.2
+# α = 0.3: thin = 30, ϵ = 1.3
+# α = 0.4: thin = 30, ϵ = 1.5
+# α = 0.5: thin = 30, ϵ = 1.5
+# α = 0.6: thin = 30, ϵ = 1.6
+# α = 0.7: thin = 30, ϵ = 1.6
+# α = 0.8: thin = 30, ϵ = 1.4
+# α = 0.9: thin = 10, ϵ = .6
 
-par = Sampler(y, X, 0.9, 50000, 20, 10000);
+par = Sampler(y, X, 0.5, 90000, 30, 10000);
 βinit = [-0.48, -0.14, -2.6, 3.7, 0., 0.1, 1.75, -0.05, 0.28]
-β, θ, σ = mcmc(par, 10000., 1., .7, βinit, 1.5, 1.1);
+β, θ, σ = mcmc(par, 10000., 1., 1.5, βinit, 1.6, 1.1);
 inits = [median(β[:,i]) for i in 1:9]
 println(inits)
 
 ##
-using Turing, StatsPlots
 chain = Chains(β, ["intercept";names(dat[:, Not(["osvAll"])])]);
-summaries = summarystats(chain)
 mean(summarystats(chain)[:, :ess]) / length(θ)
-
-chain2 = Chains([θ σ], ["θ", "σ"]);
-summaries2 = summarystats(chain2)
-
-(length(θ) / (1+2*sum(autocor(β[:,1])))) / length(θ)
-plot(chain)
-plot(chain2)
-
 
 ##
 p = 3
--- Tests/quantile/MultiProc.jl
+++ Tests/quantile/MultiProc.jl
@@ -15,54 +15,25 @@ X = X[y.>0,:];
 y = y[y.>0];
 X = hcat([1 for i in 1:length(y)], X);
 
-β, θ, σ = bootstrap(log(1), y, X, 0.9, 100)
-
-mean(θ)
-[mean(β[:,i]) for i in 1:9]
-
-
-## FREQUENTIST APPROACH
-α = range(0.1, 0.8, length = 8)
-θinit = range(2.7, 1.4, length = 8)
-
-N = 1000
-β = SharedArray{Float64}((N, size(X)[2], length(α)))
-σ = SharedArray{Float64}((N, length(α)))
-θ = SharedArray{Float64}((N, length(α)))
-
-@sync @distributed for i = 1:(length(α))
-    βt,θt,σt =  bootstrap(log(θinit[i]), y, X, α[i], N)
-    β[:,:, i] = βt
-    σ[:,i] = σt
-    θ[:,i] = θt
-end
-
-mean(θ[:, ])
-[mean(θ[:,i]) for i in 1:length(α)] |> println
-
-[mean(β[:,i,1]) for i in 1:9]
-
-βt,θt,σt =  bootstrap(log(θinit[5]), ry, X, α[5], N)
-[mean(βt[:,i]) for i in 1:9] |> println
-## BAYESIAN APPROACH
 βinit = [-0.48, -0.14, -2.6, 3.7, 0., 0.1, 1.75, -0.05, 0.28]
-M = 20
+M = 5
 p = 0.05
 feMCMC = FormatExpr("mcmc_{}.csv")
 α = range(0.1, 0.9, length = 9)
+ϵ = [0.9, 1.2, 1.3, 1.5, 1.5, 1.6, 1.6, 1.4, 0.6]
 
-for i in 1:length(α)
-    par = Sampler(y, X, α[i], 110000, 10, 10000);
+for i ∈ 1:length(α)
+    par = Sampler(y, X, α[i], 130000, 30, 10000);
     N = Integer((par.nMCMC - par.burnIn)/par.thin) + 1
     β = SharedArray{Float64}((N, size(X)[2], M))
     σ = SharedArray{Float64}((N,M))
     θ = SharedArray{Float64}((N,M))
 
-    @sync @distributed for i = 1:M
-        βt,θt,σt = mcmc(par, 100., 1., ϵ[i], βinit, 1., 1.)
-        β[:,:,i] = βt
-        θ[:,i] = θt
-        σ[:,i] = σt
+    @sync @distributed for j ∈ 1:M
+        βt,θt,σt = mcmc(par, 10000., 1., ϵ[i], βinit, 1., 1.)
+        β[:,:,j] = βt
+        θ[:,j] = θt
+        σ[:,j] = σt
     end
 
     a = DataFrame(param = colnames,

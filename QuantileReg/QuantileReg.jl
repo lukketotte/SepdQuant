458df518a13e53df26120fcb48167a322107ae5b
lukketotte <lukas.arnroth@gmail.com>
2021-10-12 13:57:43 +0200

    changing parametrization of the likelihood

--- QuantileReg/QuantileReg.jl
+++ QuantileReg/QuantileReg.jl
@@ -26,63 +26,54 @@ Sampler(y::AbstractVector{<:Real}, X::AbstractMatrix{<:Real}, nMCMC::Int) = Samp
 data(s::Sampler) = (s.y, s.X)
 param(s::Sampler) = (s.y, s.X, s.α)
 
-function δ(α::Real, θ::Real)
-    return 2*(α*(1-α))^θ / (α^θ + (1-α)^θ)
+function πθ(θ::Real)
+    θ^(-3/2) * √((1+1/θ) * trigamma(1+1/θ))
 end
 
 function θcond(s::Sampler, θ::Real, β::AbstractVector{<:Real})
     z  = s.y-s.X*β
     n = length(z)
-    a = δ(s.α, θ)*(sum((.-z[z.<0]).^θ)/s.α^θ + sum(z[z.>=0].^θ)/(1-s.α)^θ)
-    return n/θ * log(δ(s.α, θ))  - n*log(gamma(1+1/θ)) - n*log(a)/θ + loggamma(n/θ)# + logpdf(Beta(2, 2), θ/2)
+    a = gamma(1+1/θ)^θ * (sum((.-z[z.<0]).^θ)/s.α^θ + sum(z[z.>0].^θ)/(1-s.α)^θ)
+    return -log(θ) + loggamma(n/θ) - (n/θ) * log(a) + log(πθ(θ))
 end
 
 function sampleθ(s::Sampler, θ::Real, β::AbstractVector{<:Real}, ε::Real)
-    prop = rand(Uniform(maximum([0., θ-ε]), minimum([30., θ + ε])), 1)[1]
-    #prop = rand(Truncated(Normal(θ, ε^2), 0., 2.), 1)[1]
-    # logpdf(Truncated(Normal(prop, ε^2), 0., 2.), θ) - logpdf(Truncated(Normal(θ, ε^2), 0., 2.), prop)
-    return θcond(s, prop, β) - θcond(s, θ, β) >= log(rand(Uniform(0,1), 1)[1]) ? prop : θ
+    prop = rand(Truncated(Normal(θ, ε^2), 0.5, Inf), 1)[1]
+    a = logpdf(Truncated(Normal(prop, ε^2), 0.5, Inf), θ) - logpdf(Truncated(Normal(θ, ε^2), 0.5, Inf), prop)
+    return θcond(s, prop, β) - θcond(s, θ, β) + a >= log(rand(Uniform(0,1), 1)[1]) ? prop : θ
 end
+
 
 function sampleσ(s::Sampler, θ::Real, β::AbstractVector{<:Real})
     z = s.y - s.X*β
-    n = length(z)
-    b = (δ(s.α, θ) * sum((.-z[z.<0]).^θ) / s.α^θ) + (δ(s.α, θ) * sum(z[z.>=0].^θ) / (1-s.α)^θ)
-    return rand(InverseGamma(n/θ + 1, b + 1), 1)[1]
+    b = gamma(1+1/θ)^θ * (sum((.-z[z.<0]).^θ) / s.α^θ + sum(z[z.>0].^θ) / (1-s.α)^θ)
+    return (rand(InverseGamma(length(z)/θ, b), 1)[1])^(1/θ)
 end
 
-
-function logβCond(β::AbstractVector{<:Real},s::Sampler, θ::Real, σ::Real, τ::Real, λ::AbstractVector{<:Real})
+function logβCond(β::AbstractVector{<:Real}, s::Sampler, θ::Real, σ::Real)
     z = s.y - s.X*β
-    b = δ(s.α, θ)/σ * (sum((.-z[z.< 0]).^θ) / s.α^θ + sum(z[z.>=0].^θ) / (1-s.α)^θ)
-    return -b -1/(2*τ) * β'*diagm(λ.^(-2))*β
+    return - gamma(1+1/θ)^θ/σ * (sum((.-z[z.< 0]).^θ) / s.α^θ + sum(z[z.>0].^θ) / (1-s.α)^θ)
 end
 
 # first derivative
-∂β(β::AbstractVector{<:Real}, s::Sampler, θ::Real, σ::Real, τ::Real, λ::AbstractVector{<:Real}) = ForwardDiff.gradient(β -> logβCond(β, s, θ, σ, τ, λ), β)
+∂β(β::AbstractVector{<:Real}, s::Sampler, θ::Real, σ::Real) = ForwardDiff.gradient(β -> logβCond(β, s, θ, σ), β)
 # -Hessian
-∂β2(β::AbstractVector{<:Real}, s::Sampler, θ::Real, σ::Real, τ::Real, λ::AbstractVector{<:Real}) = ForwardDiff.jacobian(β -> -∂β(β, s, θ, σ, τ, λ), β)
+∂β2(β::AbstractVector{<:Real}, s::Sampler, θ::Real, σ::Real) = ForwardDiff.jacobian(β -> -∂β(β, s, θ, σ), β)
 
-function sampleβ(β::AbstractVector{<:Real}, ε::Real,  s::Sampler, θ::Real, σ::Real, τ::Real)
-    λ = abs.(rand(Cauchy(0,1), length(β)))
-    ∇ = ∂β(β, s, θ, σ, τ, λ)
-    H = (∂β2(β, s, maximum([θ, 1.0001]), σ, τ, λ))^(-1) |> Symmetric
+function sampleβ(β::AbstractVector{<:Real}, ε::Real,  s::Sampler, θ::Real, σ::Real)
+    ∇ = ∂β(β, s, θ, σ)
+    H = (∂β2(β, s, maximum([θ, 1.0001]), σ))^(-1) |> Symmetric
     prop = β + ε^2 * H / 2 * ∇ + ε * √H * vec(rand(MvNormal(zeros(length(β)), 1), 1))
-    ∇ₚ = ∂β(prop, s, θ, σ, τ, λ)
-    Hₚ = (∂β2(prop, s, maximum([θ, 1.0001]), σ, τ, λ))^(-1) |> Symmetric
-    αᵦ = logβCond(prop, s, θ, σ, τ, λ) - logβCond(β, s, θ, σ, τ, λ)
-    αᵦ += - logpdf(MvNormal(β + ε .^2 / 2 * ∇, ε^2 * H), prop) + logpdf(MvNormal(prop + ε^2/2 * ∇ₚ, ε^2 * Hₚ), β)
+    ∇ₚ = ∂β(prop, s, θ, σ)
+    Hₚ = (∂β2(prop, s, maximum([θ, 1.0001]), σ))^(-1) |> Symmetric
+    αᵦ = logβCond(prop, s, θ, σ) - logβCond(β, s, θ, σ)
+    αᵦ += - logpdf(MvNormal(β + ε^2 / 2 * ∇, ε^2 * H), prop) + logpdf(MvNormal(prop + ε^2/2 * ∇ₚ, ε^2 * Hₚ), β)
     return αᵦ > log(rand(Uniform(0,1), 1)[1]) ? prop : β
 end
 
-function sampleβ(β::AbstractVector{<:Real}, ε::AbstractVector{<:Real},  s::Sampler, θ::Real, σ::Real, τ::Real)
-    λ = abs.(rand(Cauchy(0,1), length(β)))
-    prop = vec(rand(MvNormal(β, diagm(ε)), 1))
-    return logβCond(prop, X, y, α, θ, σ, τ, λ) - logβCond(β, X, y, α, θ, σ, τ, λ) > log(rand(Uniform(0,1), 1)[1]) ? prop : β
-end
 
 #AEPD
-function mcmc(s::Sampler, τ::Real, ε::Real, εᵦ::Union{Real, AbstractVector{<:Real}}, σ₁::Real, θ₁::Real,
+function mcmc(s::Sampler, ε::Real, εᵦ::Union{Real, AbstractVector{<:Real}}, σ₁::Real, θ₁::Real,
     β₁::Union{AbstractVector{<:Real}, Nothing} = nothing; verbose = true)
     n, p = size(s.X)
     σ₁ > 0 || θ₁ > 0 || throw(DomainError("Shape ands scale must be positive"))
@@ -97,21 +88,21 @@ function mcmc(s::Sampler, τ::Real, ε::Real, εᵦ::Union{Real, AbstractVector{
 
     for i ∈ 2:s.nMCMC
         verbose && next!(p; showvalues=[(:iter,i) (:θ, round(θ[i-1], digits = 3)) (:σ, round(σ[i-1], digits = 3))])
-        mcmcInner!(s, θ, σ, β, i, ε, εᵦ, τ)
+        mcmcInner!(s, θ, σ, β, i, ε, εᵦ)
     end
     return mcmcThin(θ, σ, β, s)
 end
 
 function mcmcInner!(s::Sampler, θ::AbstractVector{<:Real}, σ::AbstractVector{<:Real},
-    β::AbstractMatrix{<:Real}, i::Int, ε::Real, εᵦ::Real, τ::Real)
+    β::AbstractMatrix{<:Real}, i::Int, ε::Real, εᵦ::Real)
         θ[i] = sampleθ(s, θ[i-1], β[i-1,:], ε)
         σ[i] = sampleσ(s, θ[i-1], β[i-1,:])
-        β[i,:] = sampleβ(β[i-1,:], εᵦ, s, θ[i], σ[i], τ)
+        β[i,:] = sampleβ(β[i-1,:], εᵦ, s, θ[i], σ[i])
         nothing
 end
 
 # ALD
-function mcmc(s::Sampler, τ::Real, εᵦ::Union{Real, AbstractVector{<:Real}},
+function mcmc(s::Sampler, εᵦ::Union{Real, AbstractVector{<:Real}},
     β₁::Union{AbstractVector{<:Real}, Nothing} = nothing, σ₁::Real = 1; verbose = true)
     n, p = size(s.X)
     σ₁ > 0 || throw(DomainError("Shape ands scale must be positive"))
@@ -126,15 +117,15 @@ function mcmc(s::Sampler, τ::Real, εᵦ::Union{Real, AbstractVector{<:Real}},
 
     for i ∈ 2:s.nMCMC
         verbose && next!(p; showvalues=[(:iter,i) (:σ, round(σ[i-1], digits = 3))])
-        mcmcInner!(s, 1, σ, β, i, εᵦ, τ)
+        mcmcInner!(s, 1, σ, β, i, εᵦ)
     end
     return mcmcThin(σ, β, s)
 end
 
 function mcmcInner!(s::Sampler, σ::AbstractVector{<:Real},
-    β::AbstractMatrix{<:Real}, i::Int, εᵦ::Real, τ::Real)
+    β::AbstractMatrix{<:Real}, i::Int, εᵦ::Real)
         σ[i] = sampleσ(s, 1, β[i-1,:])
-        β[i,:] = sampleβ(β[i-1,:], εᵦ, s, 1, σ[i], τ)
+        β[i,:] = sampleβ(β[i-1,:], εᵦ, s, 1, σ[i])
         nothing
 end
 
--- Tests/quantile/debug.jl
+++ Tests/quantile/debug.jl
@@ -1,79 +1,22 @@
 using Distributions, LinearAlgebra, StatsBase, SpecialFunctions, ForwardDiff
-using Plots, PlotThemes, CSV, DataFrames, StatFiles, CSVFiles
+using Plots, PlotThemes
 theme(:juno)
-using Turing, QuantileRegressions
-include("../../QuantileReg/QuantileReg.jl")
+
 include("../aepd.jl")
-using .QuantileReg, .AEPD
-using KernelDensity
+include("../../QuantileReg/QuantileReg.jl")
+using .AEPD, .QuantileReg
 
-## Half cauchy check
-sims = abs.(rand(Cauchy(0,1), 100))
-k = kde(sims);
-x = range(minimum(sims), maximum(sims), length = 500);
-plot(x, pdf(k, x))
 
-##
 n = 200;
 β, α, σ = [2.1, 0.8], 0.5, 2.;
 θ =  1.
 X = [repeat([1], n) rand(Uniform(10, 20), n)]
-y = X * β .+ rand(Aepd(0., σ, θ, 0.5), n);
+y = X * β .+ rand(Normal(0., 1.), n);
 
-##
-function δ(α::Real, θ::Real)
-    return 2*(α*(1-α))^θ / (α^θ + (1-α)^θ)
-end
+α = 0.5
+par = Sampler(y, X, .5, 10000, 1, 1);
+β, θ, σ = mcmc(par, 0.5, 0.5, 2, 2);
 
-function logβCond(β::AbstractVector{<:Real}, s::Sampler, θ::Real, σ::Real, τ::Real, λ::AbstractVector{<:Real})
-    z = s.y - s.X*β
-    b = δ(s.α, θ)/σ * (sum((.-z[z.< 0]).^θ) / s.α^θ + sum(z[z.>=0].^θ) / (1-s.α)^θ)
-    return -b -1/(2*τ) * β'*diagm(λ.^(-2))*β
-end
-
-# first derivative
-∂β(β::AbstractVector{<:Real}, s::Sampler, θ::Real, σ::Real, τ::Real, λ::AbstractVector{<:Real}) = ForwardDiff.gradient(β -> logβCond(β, s, θ, σ, τ, λ), β)
-∂β2(β::AbstractVector{<:Real}, s::Sampler, θ::Real, σ::Real, τ::Real, λ::AbstractVector{<:Real}) = ForwardDiff.jacobian(β -> -∂β(β, s, θ, σ, τ, λ), β)
-
-##
-diagm([1; [10 for i in 1:5]])^(-1)
-
-function hmc(β::AbstractArray{Float64}, θ::Real, σ::Real, τ::Real, s::Sampler; L = 40, ϵ = 0.001, p = size(s.X, 2))
-    λ = abs.(rand(Cauchy(0,1), p))
-    #M = diagm([1; [10 for i in 1:(p-1)]])^(-1) |> Symmetric
-    M = diagm([.3, 1, .5, .7, 1000, 1, .5, 1000, .1])^(-1)
-    ϕ = reshape(rand(MvNormal(zeros(p), M), 1), p) + 0.5 * ϵ * ∂β(β, s, θ, σ, τ, λ)
-    β₁, ϕ₁ = β, ϕ
-    log_p = logβCond(β₁, s, θ, σ, τ, λ) - ϕ₁'*M*ϕ₁/2 # sum(ϕ₁.^2)/2
-    for l ∈ 1:L
-        ϕ += 0.5 * ϵ * ∂β(β, s, θ, σ, τ, λ)
-        β += ϵ * M * ϕ
-        ϕ += 0.5 * ϵ * ∂β(β, s, θ, σ, τ, λ)
-    end
-    log_p_L = logβCond(β, s, θ, σ, τ, λ) - ϕ'*M*ϕ/2 # sum(ϕ.^2)/2
-    r = exp(log_p_L - log_p)
-    r > rand(Uniform()) ? β : β₁
-end
-
-par = Sampler(y, X, 0.5, 1000, 1, 1);
-inits = coef(qreg(@formula(y ~ x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9), hcat(DataFrame(y = log.(y)), DataFrame(X, :auto)), 0.5))
-
-N = 4001
-b = zeros(N, size(X,2))
-b[1,:] = inits
-for i ∈ 2:N
-    b[i,:] = hmc(b[i-1,:], 1.8, 4.5, 100, par; L = 40, ϵ = 0.001)
-end
-p = 8
-plot(β[:,p])
-plot!(b[:,p])
-
-p = 1
-plot(1:N, cumsum(b[:,p])./(1:N))
-plot!(1:N, cumsum(β[:,p])./(1:N))
-
-1-((b[2:N, 2] .=== b[1:(N - 1), 2]) |> mean)
-chain = Chains(b, ["intercept";names(dat[:, Not(["osvAll"])])]);
-mean(summarystats(chain)[:, :ess]) / N
-[median(b[:,i]) for i in 1:size(X,2)]
-println(inits)
+plot(θ)
+plot(σ)
+plot(β[:,1])

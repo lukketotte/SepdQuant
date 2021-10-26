8cefebcf736d243ce48b2f0dd71164422078b072
lukketotte <lukas.arnroth@gmail.com>
2021-10-26 10:55:38 +0200

    not sure

--- Tests/quantile/ConflictDat.jl
+++ Tests/quantile/ConflictDat.jl
@@ -27,38 +27,30 @@ X = hcat([1 for i in 1:length(y)], X);
 # α = 0.8: thin = 30, ϵ = 1.4
 # α = 0.9: thin = 10, ϵ = .6
 
-α = 0.9
-par = Sampler(y, X, α, 1000, 5, 1);
+α = 0.05
+par = Sampler(y, X, α, 21000, 5, 1000);
 
 βinit = DataFrame(hcat(par.y, par.X), :auto) |> x ->
     qreg(@formula(x1 ~  x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10), x, α) |> coef
 #par = Sampler(y, X, 0.7, 200000, 5, 100000);
-β, θ, σ = mcmc(par, 0.4, 0.2, 1., 1., βinit);
+β, θ, σ = mcmc(par, 0.4, 0.4, 1., 1., βinit);
 #β, θ, σ = mcmc(par, 1000., .8, 0.4, 0.9, 1.1, βinit);
 #βa, σa = mcmc(par, 1000., 0.5, βinit, 1);
 
 1-((β[2:size(β, 1), 1] .=== β[1:(size(β, 1) - 1), 1]) |> mean)
 
-acceptance(θ::AbstractMatrix{<:Real}) = size(θ, 1) |> n -> 1-((θ[2:n, 1] .=== θ[1:(n - 1), 1]) |> mean)
-acceptance(θ::AbstractVector{<:Real}) = length(θ) |> n -> 1-((θ[2:n] .=== θ[1:(n - 1), 1]) |> mean)
-
-acceptance(β)
-acceptance(θ)
 
 mean(β, dims = 1)
 βinit
 p = 1
 plot(β[:, 7])
-#plot(βa[:, 2])
 plot(θ)
-plot(cumsum(β[:,9])./(1:length(θ)))
+plot(cumsum(β[:,2])./(1:length(θ)))
 
-b = [mean(β[:,i]) for i in 1:9]
-println(b)
 
 Q = zeros(length(y))
 for i ∈ 1:length(y)
-    Q[i] = y[i] <= ceil(exp(X[i,:] ⋅ b) + 0.5)
+    Q[i] = y[i] <= ceil(exp(X[i,:] ⋅ b) + par.α)
 end
 mean(Q)
 mean(y <= ceil.(exp.(X * b) .+ 0.5))
